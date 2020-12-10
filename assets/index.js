006af3f885707c58d9340ff0cb6dd3f9a66c88e7
nodari-dev <nodar2206@gmail.com>
2020-12-10 18:33:31 +0200

    Blog list, collection pages, product elements, new header style, bug fix with 4 el per row

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+console.log('test');
+var PostContent = $('div.post-info');
+$( ".post-info > p" ).has( "iframe" ).addClass("video-post");
+$( ".post-info > p" ).has( "img" ).addClass("image-post");
