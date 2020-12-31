42ea872d833d441940287710d24ba0596e3d1001
nodari-dev <nodar2206@gmail.com>
2021-01-01 00:08:57 +0200

    New year update

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,19 @@
-console.log('test');
 var PostContent = $('div.post-info');
-$( ".post-info > p" ).has( "iframe" ).addClass("video-post");
-$( ".post-info > p" ).has( "img" ).addClass("image-post");
+$(".post-info > p").has("iframe").addClass("video-post");
+$(".post-info > p").has("img").addClass("image-post");
+$("#read-more").on('click', function() {
+    $(".product-description-block").addClass('read-mode');
+    $("body").addClass('no-scroll');
+});
+$(".disable-read-mode").on('click', function() {
+    $(".product-description-block").removeClass('read-mode');
+    $("body").removeClass('no-scroll');
+});
+$(".open-menu").on('click', function() {
+    $(".header-menu").removeClass('hidden');
+    $("body").addClass('no-scroll');
+});
+$(".close-menu").on('click', function() {
+    $(".header-menu").addClass('hidden');
+    $("body").removeClass('no-scroll');
+});
